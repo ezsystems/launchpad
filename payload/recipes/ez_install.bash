f488278621bbbfdda5af2cfbc8a18b3429aad793
Morel Sébastien <morel.seb@gmail.com>
2017-04-11 16:58:07 -0700

    Manage http basics

---
+++ payload/recipes/ez_install.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+PHP="php"
+COMPOSER="$PHP -d memory_limit=-1 composer.phar"
+
+
+
+REPO=$1
+VERSION=$2
+
+
+
+echo "Installation eZ Platform ($REPO:$VERSION) in the container"
+# Set up Auth
+
+
+$COMPOSER config http-basic.updates.ez.no <installation-key> <token-password>
+
+# Install
+$COMPOSER create-project --no-dev --no-interaction $REPO ezplatform $VERSION
+cp composer.phar ezplatform
+cd ezplatform
+
+
+# Wait for the DB
+while ! mysqladmin ping -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" --silent; do
+    echo "."
+    sleep 1
+done
+echo ""
+
+$PHP app/console doctrine:database:create
+$PHP app/console ezplatform:install clean
+
+echo "Installation OK"
+
