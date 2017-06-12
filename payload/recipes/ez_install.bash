38383928c38e98d65019c01560b2792ca0edfe70
Morel Sébastien <morel.seb@gmail.com>
2017-06-12 15:17:59 -0700

    bug in the install

--- payload/recipes/ez_install.bash
+++ payload/recipes/ez_install.bash
@@ -8,17 +8,16 @@ REPO=$1
 VERSION=$2
 INIT_DATA=$3
 
-CONSOLE="bin/console"
-if [ -f ezplatform/app/console ]; then
-    CONSOLE="app/console"
-fi
-
 echo "Installation eZ Platform ($REPO:$VERSION:$INIT_DATA) in the container"
 # Install
 $COMPOSER create-project --no-interaction $REPO ezplatform $VERSION
 cp composer.phar ezplatform
 cd ezplatform
 
+CONSOLE="bin/console"
+if [ -f app/console ]; then
+    CONSOLE="app/console"
+fi
 
 # Wait for the DB
 while ! mysqladmin ping -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" --silent; do
