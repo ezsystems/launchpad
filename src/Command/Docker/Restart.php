3f26d94daea02a6c573bfe9c57258533c820f854
Ahmed EBEN HASSINE <ahmedbhs123@gmail.com>
2021-01-22 22:35:49 +0100

    fix: update restart command name

--- src/Command/Docker/Restart.php
+++ src/Command/Docker/Restart.php
@@ -17,14 +17,14 @@ final class Restart extends DockerCommand
     protected function configure(): void
     {
         parent::configure();
-        $this->setName('docker:start')->setDescription('Restart all the services (or just one).');
+        $this->setName('docker:restart')->setDescription('Restart all the services (or just one).');
         $this->addArgument('service', InputArgument::OPTIONAL, 'Service to restart', '');
         $this->setAliases(['restart']);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->dockerClient->start($input->getArgument('service'));
+        $this->dockerClient->restart($input->getArgument('service'));
 
         return DockerCommand::SUCCESS;
     }
